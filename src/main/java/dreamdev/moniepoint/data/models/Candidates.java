3b3f9732a091a9345c66ddf936094c2cdc4f2044
Tes-program <teslimodumuyiwa@gmail.com>
2026-03-29 10:34:45 +0100

    [FEAT]: Candidates and Votes

--- src/main/java/dreamdev/moniepoint/data/models/Candidates.java
+++ src/main/java/dreamdev/moniepoint/data/models/Candidates.java
@@ -1,4 +1,15 @@
 package dreamdev.moniepoint.data.models;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document()
 public class Candidates {
+    @Id
+    private String id;
+    private String name;
+    private String electionId;
+    private Integer voteCount;;
 }
--- src/main/java/dreamdev/moniepoint/data/models/Votes.java
+++ src/main/java/dreamdev/moniepoint/data/models/Votes.java
@@ -1,4 +1,17 @@
 package dreamdev.moniepoint.data.models;
+import lombok.Data;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Data
+@Document()
 public class Votes {
+    @Id
+    private String id;
+    private String electionId;
+    private String candidateId;
+    private String voterId;
+    private String createdAt;
 }
